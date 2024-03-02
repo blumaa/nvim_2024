fea61f11fb57c323169b6a5a9f5470bb139bb29c
aaron blum <blumaa@gmail.com>
2024-03-02 23:11:37 +0100

    remove unused plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -12,12 +12,12 @@ return {
     config = true
   },
 
-  {
-    'eandrju/cellular-automaton.nvim',
-    config = function()
-      vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
-    end
-  },
+  -- {
+  --   'eandrju/cellular-automaton.nvim',
+  --   config = function()
+  --     vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+  --   end
+  -- },
 
   {
     "crnvl96/lazydocker.nvim",
