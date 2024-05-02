9eeed08ee6d2d75ef4b1fcac7755dd920aefeb4f
aaron blum <blumaa@gmail.com>
2024-05-02 23:25:22 +0200

    blankline

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,6 +7,9 @@ return {
   },
   config = function()
     require('nvim-tree').setup({
+      view = {
+        adaptive_size = true,
+      },
       actions = {
         open_file = {
           quit_on_open = true,
