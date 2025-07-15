54a7b80bd9be6b5ad6fe5b99e474b78cab435893
aaron blum <blumaa@gmail.com>
2025-07-15 09:45:04 +0200

    add-vue-lsp

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,38 @@
+return {
+  'stevearc/conform.nvim',
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  keys = {
+    {
+      "<leader>P",
+      function()
+        require("conform").format({ async = true, lsp_fallback = true })
+      end,
+      mode = "",
+      desc = "Format buffer",
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      javascript = { "prettier" },
+      typescript = { "prettier" },
+      javascriptreact = { "prettier" },
+      typescriptreact = { "prettier" },
+      vue = { "prettier" },
+      css = { "prettier" },
+      html = { "prettier" },
+      json = { "prettier" },
+      yaml = { "prettier" },
+      markdown = { "prettier" },
+      lua = { "stylua" },
+    },
+    default_format_opts = {
+      lsp_fallback = true,
+    },
+    format_on_save = {
+      -- These options will be passed to conform.format()
+      timeout_ms = 500,
+      lsp_fallback = true,
+    },
+  },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,113 +19,94 @@ return {
       cmp_lsp.default_capabilities()
     )
     require('mason').setup({})
-    require("mason-lspconfig").setup {
-      ensure_installed = {
-        "lua_ls",
-        "jsonls",
-        "cssls",
-        "html",
-        "yamlls",
-        "dockerls",
-        "eslint",
-        "stylelint_lsp",
-        "vuels"
-      },
-      handlers = {
-        function(server_name) -- default handler (optional)
-          -- print('setting up ' .. server_name)
-          --      if server_name == "tsserver" then
-          -- 	server_name = "ts_ls"
-          -- end
-
-          require("lspconfig")[server_name].setup {
-            capabilities = capabilities,
-          }
-        end,
-        ["ts_ls"] = function()
-          require("lspconfig").ts_ls.setup({
-            capabilities = capabilities,
-            -- root_dir = vim.loop.cwd,
-            init_options = {
-              plugins = {
-                {
-                  name = "typescript-lit-html-plugin",
-                  -- location = vim.env.NODE_LIB,
-                  location = "/Users/a.blum/.asdf/installs/nodejs/20.6.1/lib/node_modules/typescript-lit-html-plugin",
-                },
-                {
-                  name = "ts-lit-plugin",
-                  -- location = vim.env.NODE_LIB,
-                  location = "/Users/a.blum/.asdf/installs/nodejs/20.6.1/lib/node_modules/ts-lit-plugin",
-                },
-              },
-            },
-          })
-        end,
-        ["lua_ls"] = function()
-          local lspconfig = require("lspconfig")
-          lspconfig.lua_ls.setup {
-          }
-          lspconfig.lua_ls.setup {
-            on_init = function(client)
-              if client.workspace_folders then
-                local path = client.workspace_folders[1].name
-                if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
-                  return
-                end
+    
+    -- Setup LSP servers directly without automatic_enable
+    local lspconfig = require('lspconfig')
+    
+    -- Setup servers manually
+    local servers = {
+      "lua_ls",
+      "jsonls", 
+      "cssls",
+      "html",
+      "yamlls",
+      "dockerls",
+      "eslint",
+      "stylelint_lsp",
+      "vue-language-server"
+    }
+    
+    for _, server in ipairs(servers) do
+      if server == "lua_ls" then
+        -- Custom lua_ls setup (keeping your existing config)
+        lspconfig.lua_ls.setup {
+          on_init = function(client)
+            if client.workspace_folders then
+              local path = client.workspace_folders[1].name
+              if vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc') then
+                return
               end
+            end
 
-              client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = {
-                    vim.env.VIMRUNTIME
-                    -- Depending on the usage, you might want to add additional paths here.
-                    -- "${3rd}/luv/library"
-                    -- "${3rd}/busted/library",
-                  }
-                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                  -- library = vim.api.nvim_get_runtime_file("", true)
+            client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+              runtime = {
+                version = 'LuaJIT'
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME
                 }
-              })
-            end,
-            capabilities = capabilities,
-            settings = {
-              Lua = {
-                diagnostics = {
-                  globals = { "vim" }
-                }
+              }
+            })
+          end,
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { "vim" }
               }
             }
           }
-        end,
-        -- ["stylelint_lsp"] = function()
-        --   require("lspconfig").stylelint_lsp.setup {
-        --     capabilities = capabilities,
-        --     filetypes = { "css", "scss", "less" },
-        --     settings = {
-        --       stylelintplus = {
-        --         autoFixOnSave = false,
-        --         autoFixOnFormat = false,
-        --         configFile = ".stylelintrc.json" -- Adjust this path if needed
-        --       }
-        --     }
-        --   }
-        -- end,
-        -- ["prettier"] = function()
-        --   require("lspconfig").prettierd.setup {
-        --     capabilities = capabilities,
-        --     filetypes = { "css", "scss", "less", "javascript", "typescript", "json" },
-        --   }
-        -- end,
-      }
-    }
+        }
+      elseif server == "ts_ls" then
+        -- TypeScript server
+        lspconfig.ts_ls.setup({
+          capabilities = capabilities,
+          init_options = {
+            plugins = {
+              {
+                name = "typescript-lit-html-plugin",
+                location = "/Users/a.blum/.asdf/installs/nodejs/20.6.1/lib/node_modules/typescript-lit-html-plugin",
+              },
+              {
+                name = "ts-lit-plugin",
+                location = "/Users/a.blum/.asdf/installs/nodejs/20.6.1/lib/node_modules/ts-lit-plugin",
+              },
+            },
+          },
+        })
+      elseif server == "vue-language-server" then
+        -- Vue language server (Volar) in takeover mode
+        lspconfig.volar.setup({
+          capabilities = capabilities,
+          filetypes = {'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue'},
+          init_options = {
+            vue = {
+              hybridMode = false,
+            },
+          },
+        })
+      else
+        -- Default setup for other servers
+        lspconfig[server].setup {
+          capabilities = capabilities,
+        }
+      end
+    end
+    
+    -- ts_ls setup is handled above in the loop now
+    
     require("fidget").setup({})
   end
 }
