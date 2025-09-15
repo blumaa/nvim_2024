666baf6b25ad5a05b8f028650dbe2d048412e5f9
aaron blum <blumaa@gmail.com>
2025-09-15 12:20:59 +0200

    update incline

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -22,11 +22,19 @@ return {
         end
         local ft_icon, ft_color = devicons.get_icon_color(parent_folder_and_file)
         local modified = vim.bo[props.buf].modified
+
+        -- Get cursor position and file percentage
+        local line = vim.api.nvim_win_get_cursor(props.win or 0)[1]
+        local col = vim.api.nvim_win_get_cursor(props.win or 0)[2] + 1
+        local total_lines = vim.api.nvim_buf_line_count(props.buf)
+        local percentage = math.floor((line / total_lines) * 100)
+        local position_info = string.format(' %d,%d %d%% ', line, col, percentage)
+
         return {
           ft_icon and { ' ', ft_icon, ' ', guibg = ft_color, guifg = helpers.contrast_color(ft_color) } or '',
           ' ',
           { parent_folder_and_file .. ' ', gui = modified and 'bold,italic' or 'bold' },
-          ' ',
+          { position_info, guifg = '#a6accd' },
           guibg = '#44406e',
         }
       end,
